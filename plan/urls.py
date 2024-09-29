a3ce441ec52771aedcc37ecafdd2aad204dbcfb2
Ananthu krishnan <ananthukrishnanr22597@gmail.com>
2024-09-29 15:16:15 +0530

    removed unwanted apps

--- plan/urls.py
+++ plan/urls.py
@@ -22,7 +22,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("accounts/", include("accounts.urls")),
-    path("", include("tasks.urls")),
     path("__reload__/", include("django_browser_reload.urls")),
 ]
 
